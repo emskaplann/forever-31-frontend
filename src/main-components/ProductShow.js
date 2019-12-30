057a289b133c6f7dc5dee66df3ffa0e487097c9a
Emirhan Kaplan <emskaplann@gmail.com>
2019-12-30 10:58:33 -0500

    savepoint

--- src/main-components/ProductShow.js
+++ src/main-components/ProductShow.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Container, Row, Col } from 'react-bootstrap';
-import { Dimmer, Header, Divider, Loader } from 'semantic-ui-react';
+import { Dimmer, Header, Divider, Loader, Card, Image } from 'semantic-ui-react';
+import ReactHtmlParser from 'react-html-parser';
+
 
 class ProductShow extends React.Component {
   constructor () {
@@ -11,17 +13,48 @@ class ProductShow extends React.Component {
     }
   }
 
+  componentDidMount () {
+
+  }
+
   render () {
+    const product = this.props.product
+
     return(
-      <Container>
+      <Container style={{marginTop: 30, marginBottom: 10}}>
+        <Row>
+          <Col xs={4} sm={4} md={4} lg={4}>
+            <Card>
+              <Image src={product.images[0].front_url} />
+            </Card>
+          </Col>
+          <Col xs={4} sm={4} md={4} lg={4}>
+            { ReactHtmlParser(product.description) }
+          </Col>
+          <Col xs={4} sm={4} md={4} lg={4}>
+            <Row>
+              <Col style={{textAlign: 'left', paddingLeft: 0}} sm={true}>
+                Shipping Fee: {product.shipping_fee}
+              </Col>
+            </Row>
+            <Row>
+              <Col style={{textAlign: 'left', paddingLeft: 0}} sm={true}>
+                Student Deal: {product.student_deal ? 'Yes' : 'No'}
+              </Col>
+            </Row>
+          </Col>
+        </Row>
       </Container>
     )
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
+  // fetching the product from the state for show page
+  const productId = ownProps.match.params.id
   return {
-    user: state.user
+    user: state.user,
+    product: state.products.find(product => product.id == productId)
   }
 }
 
