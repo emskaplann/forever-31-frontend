796c71c6d8c2a9a682e3d376b924d1d9bfca3aca
Emirhan Kaplan <emskaplann@gmail.com>
2020-01-02 21:04:11 -0500

    Updates

---
+++ precache-manifest.3f4f2fa676bc73fd618bb31c819e0d2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b93245b7ab97961165f935b7dbcd477",
+    "url": "/forever-31/index.html"
+  },
+  {
+    "revision": "46bd899d7a3893fb7329",
+    "url": "/forever-31/static/css/2.74e16132.chunk.css"
+  },
+  {
+    "revision": "afef419f20c30c00fff4",
+    "url": "/forever-31/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "46bd899d7a3893fb7329",
+    "url": "/forever-31/static/js/2.b92d1ed8.chunk.js"
+  },
+  {
+    "revision": "8018c46bf6ab4e9d69e6899660f97ba8",
+    "url": "/forever-31/static/js/2.b92d1ed8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "afef419f20c30c00fff4",
+    "url": "/forever-31/static/js/main.7c878300.chunk.js"
+  },
+  {
+    "revision": "7c8a237d6a8dbb465477",
+    "url": "/forever-31/static/js/runtime-main.f31d997f.js"
+  }
+]);
