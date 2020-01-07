d2291766080f47b4bc08d382d191cd242e7af23e
Emirhan Kaplan <emskaplann@gmail.com>
2020-01-07 12:31:31 -0500

    asd

--- src/App.js
+++ src/App.js
@@ -97,7 +97,7 @@ class App extends React.Component {
               <NavbarView sendContentId={this.getContentId} openModal={this.setVisible} />
                 <Segment style={{backgroundColor: '#fcfeff'}} basic>
                       <Switch location={location}>
-                        <Route exact path='/' component={ProductIndex} key='index' />
+                        <Route exact path={process.env.PUBLIC_URL + '/'} component={ProductIndex} key='index' />
                         <Route exact path='/products/:id' render={(props) => <ProductShow {...props} openModal={this.setVisible} />} key='show' />
                         {/* Stripe Route */}
                         <StripeProvider apiKey="pk_test_YJZiIQadCitjxkefrqHysj0g00BNRtnusD">
